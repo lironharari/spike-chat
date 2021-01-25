e192a56126f06abd691ad131f3c926181756c406
Liron Harari <lironharari@gmail.com>
2021-01-25 22:02:12 +0200

    active users comments fix

--- controllers/ActiveUsers.js
+++ controllers/ActiveUsers.js
@@ -1,9 +1,9 @@
+
 /**
  * Import Models
  *  activeUser - A users that is in the chat
  *  userEntry - a message for client upon client entry (entry or exit)
  */
-
 const activeUser = require("../models/activeUser.js");
 const userEntry = require("../models/userEntry.js");
 
@@ -13,8 +13,8 @@ module.exports = function () {
     /**
      * Add a new user to the active users list.
      * @param {object} data - an object containing client message
-     */        
-    this.pushNewUser = function (data) {
+     */    
+    this.pushNewUser = (data) => {
         this.users.push(activeUser(data)); // push a new user into list
     };
 
@@ -24,24 +24,24 @@ module.exports = function () {
      * @param {object} data - an object containing user id, user name, and date of entry
      * @returns {object} - an object containing message type, a list of active users, 
      * user name, user id, and date of creation.
-     */    
-    this.createUserEntry = function (type, data) {
-        return userEntry(type, data, this.users);
-    };
+     */
+    this.createUserEntry = (type, data) => (
+        userEntry(type, data, this.users)
+    )
 
     /**
      * Remove a user from the active list
      * @param {number} id - user id
-     */
-    this.removeUser = function (id) {
+     */    
+    this.removeUser = (id) => {
         this.users = this.users.filter(user => user.id !== id); // filter a user and update list
     };
 
     /**
      * Get list of active users
      * @returns {object} - list of active users as array
-     */
-    this.getActiveUsers = function () {
-        return this.users;
-    };
+     */     
+    this.getActiveUsers = () => (
+        this.users
+    )
 }
