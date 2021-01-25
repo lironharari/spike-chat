bde163d15fa0ff1d6c15ac3c1555ad6e9b784dea
Liron Harari <lironharari@gmail.com>
2021-01-25 22:06:44 +0200

    model comments

--- models/activeUser.js
+++ models/activeUser.js
@@ -1,7 +1,12 @@
-module.exports = function ( data ) {
-    return {
+/**
+ * Create active user object
+ * @param {object} data - an object containing client message
+ * @returns {object} - an object containing user id, user name, and avatar image
+ */
+module.exports = (data) => (
+    {
         id: data.id,
         name: data.name,
         avatar: data.avatar
-    };
-}; 
+    }
+)
--- models/userEntry.js
+++ models/userEntry.js
@@ -1,9 +1,17 @@
-module.exports = function ( type, data, users ) {
-    return {
+/**
+ * Create client entry message
+ * @param {string} type - type of entry (enter or exit)
+ * @param {object} data - client message upon entry
+ * @param {object} users - a list of active users as array
+ * @returns {object} - a message for the client containing entry type (enter or exit)
+ * a list of active users, client name, client id, and date of entry
+ */
+module.exports = (type, data, users) => (
+    {
         type: type,
         users: users,
         name: data.name,
         id : data.id,
         date: data.date
-    };
-}; 
+    }
+)
