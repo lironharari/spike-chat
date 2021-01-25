8984a7ea0507c6b6a69e299e31e824510d4c5514
Liron Harari <lironharari@gmail.com>
2021-01-25 22:19:48 +0200

    added server comments

--- server.js
+++ server.js
@@ -1,15 +1,20 @@
+/**
+ * Chat - Server side
+ * Node - Express - Web Socket
+ */
 
 const express = require("express");
 const { Server } = require("ws");
-const ActiveUsers = require("./controllers/ActiveUsers.js");
-const MessageType = require("./models/messageType.js");
+const ActiveUsers = require("./controllers/ActiveUsers.js"); // active users module
+const messageType = require("./models/messageType.js"); // message types
+const messageTypeError = "Unknown Message Type";
 
 const app = express();
 const PORT = process.env.PORT || 3000;
-const INDEX = "/index.html";
-const activeUsers = new ActiveUsers();
+const INDEX = "/index.html"; // initial websocket client
+const activeUsers = new ActiveUsers(); // active users moduless
 
-app.use(express.static(__dirname + "/public")); 
+app.use(express.static(__dirname + "/public")); // expose public folder
 
 const server = app
               .use((req, res) => res.sendFile(INDEX, { root: __dirname }))   
@@ -19,28 +24,28 @@ const wss = new Server({ server });
 
 wss.on("connection", (ws) => {    
   ws.on("message", (message) => {
-    let data = JSON.parse(message);    
+    let data = JSON.parse(message); // parse message data to object
 
     switch (data.type) {
-      case MessageType.OPEN:
-        activeUsers.pushNewUser(data);        
-        wss.clients.forEach((client) => {
-          client.send(JSON.stringify(activeUsers.createUserEntry(MessageType.OPEN, data)));
+      case messageType.OPEN:
+        activeUsers.pushNewUser(data); // add active user to list
+        wss.clients.forEach((client) => { // send user entry message (new client) to all clients
+          client.send(JSON.stringify(activeUsers.createUserEntry(messageType.OPEN, data)));
         });        
         break;
-      case MessageType.MESSAGE:        
+      case messageType.MESSAGE:        
         wss.clients.forEach((client) => {
-          client.send(message);
+          client.send(message); // send chat message to all clients
         });
         break;
-      case MessageType.CLOSE:        
-        activeUsers.removeUser(data.id);        
-        wss.clients.forEach((client) => {
-          client.send(JSON.stringify(activeUsers.createUserEntry(MessageType.CLOSE, data)));
+      case messageType.CLOSE:        
+        activeUsers.removeUser(data.id); // remove user from list
+        wss.clients.forEach((client) => { // send user entry message (exit) to all clients
+          client.send(JSON.stringify(activeUsers.createUserEntry(messageType.CLOSE, data)));
         });         
         break;        
       default:
-        console.log("Unknown Message Type");
+        console.log(messageTypeError);
     }
   });
 });
